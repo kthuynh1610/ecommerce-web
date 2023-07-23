5f3372f478946ba69f7e2cb23702c3711d3c8595
Tu Huynh <khanhtupro@gmail.com>
2023-07-23 10:50:12 +1000

    set up structure for webapp pt2

--- ecommerce-web/src/App.js
+++ ecommerce-web/src/App.js
@@ -1,23 +1,23 @@
-import './App.css';
-
+//import Cart from './Components/Cart';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Header from './Components/Header/Header';
+import HomePage from './Components/HomePage/HomePage';
+import New from './Components/NewArrival/New';
+import All from './Components/AllProducts/All';
+import NewC from './Components/NewCollection/NewC';
+import Footer from './Components/Footer/Footer';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path="/newArrival" element={<New/>}/>
+        <Route path="/All" element={<All/>}/>
+        <Route path="NewCollection" element={<NewC/>}/>
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   );
 }
 
