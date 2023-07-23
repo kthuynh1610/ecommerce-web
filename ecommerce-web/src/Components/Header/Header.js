e7191b307c40034c3b113610930a8c5cf0b28c37
Tu Huynh <khanhtupro@gmail.com>
2023-07-23 15:09:03 +1000

    Header completed

--- ecommerce-web/src/Components/Header/Header.js
+++ ecommerce-web/src/Components/Header/Header.js
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import "./Header.css";
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='container'>
+        <div className='Logo'>
+            <div className='dropdown'>
+                <i class="fa-solid fa-bars" style={{color: " #000000"}}></i>
+            </div>
+            <h1>Guobin Studio</h1>
+            <div>
+                <i class="fa-solid fa-magnifying-glass" style={{color: " #000000"}}></i>
+                <i class="fa-regular fa-user" style={{color: " #000000"}}></i>
+                <i class="fa-solid fa-cart-shopping" style={{color: " #000000"}}/>
+            </div>
+        </div>
+        <ul>
+            <li>New Arrival</li>
+            <li>All Products</li>
+            <li>Sales</li>
+            <li>Spring Summer 2023</li>
+        </ul>
+    </div>
   )
 }
 
