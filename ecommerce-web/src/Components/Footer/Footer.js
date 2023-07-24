e55ac5c31af7076d3fa29053ebaff95ec0fde54e
Tu Huynh <khanhtupro@gmail.com>
2023-07-24 11:26:03 +1000

    Footer pt1

--- ecommerce-web/src/Components/Footer/Footer.js
+++ ecommerce-web/src/Components/Footer/Footer.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='container-footer'>
+      <div>
+
+      </div>
+      <div className='itemList'>
+        <ul>
+          <li>New Arrival</li>
+          <li>All product</li>
+          <li>Sale</li>
+        </ul>
+      </div>
+      <div className='copyright'>
+        <h2>Copyright © 2023 Oiduts Studio</h2>
+      </div>
+    </div>
   )
 }
 
