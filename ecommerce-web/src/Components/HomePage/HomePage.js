43863c53c17f0723c83e588e8bb01595b21d3132
Tu Huynh <khanhtupro@gmail.com>
2023-07-24 11:25:50 +1000

    auto play slideshow finished on Homepage

--- ecommerce-web/src/Components/HomePage/HomePage.js
+++ ecommerce-web/src/Components/HomePage/HomePage.js
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import SlideShow from './SlideShow/SlideShow'
+import './HomePage.css';
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className='container-Home'>
+        <SlideShow/>
+        <div className='container-item'>
+            <h1>Ready to wear</h1>
+            <div className='dot'></div>
+            <p>View more</p>
+            <div class="ItemList">
+              
+            </div>
+        </div>
+    </div>
   )
 }
 
--- ecommerce-web/src/Components/HomePage/SlideShow/SlideShow.js
+++ ecommerce-web/src/Components/HomePage/SlideShow/SlideShow.js
@@ -1,8 +1,53 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import photo1 from '../../../Assets/slideshow_1.jpeg';
+import photo2 from '../../../Assets/slideshow_2.jpeg';
+import photo3 from '../../../Assets/slideshow_3.jpeg';
+import photo4 from '../../../Assets/slideshow_4.jpeg';
+import './SlideShow.css';
+const delay = 2500;
+const photos =[
+  {
+    id: 1,
+    src: photo1
+  },
+  {
+    id: 2, 
+    src: photo2
+  },
+  {
+    id: 3, 
+    src: photo3
+  },
+  {
+    id: 4, 
+    src: photo4
+  }
+]
 const SlideShow = () => {
+  const [index, setIndex] = useState(0);
+
+  useEffect(()=>{
+    setTimeout(
+      ()=>
+       setIndex((prevIndex)=>
+        prevIndex === photos.length -1 ? 0 : prevIndex +1
+      ),
+      delay
+    )
+    return()=>{};
+  }, [index]);
   return (
-    <div>SlideShow</div>
+    <div className='container'>
+      <div className='slider'
+        style={{transform: `translate3d(${-index *100}%,0 ,0)`}}
+      >
+        {photos.map((photo)=>{
+          return <div className='slide' key={photo.id}>
+            <img src={photo.src} alt={photo.src}/>
+          </div>
+        })}
+      </div>
+    </div>
   )
 }
 
